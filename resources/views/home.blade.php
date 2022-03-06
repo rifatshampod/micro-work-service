e329f57929866fb2d64c177ac8257589ae3bdc90
rifatshampod <shampodrifat@gmail.com>
2022-03-07 03:49:49 +0800

    till controller added, configuration done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <title>PTO Worker</title>
+</head>
+<body>
+    <h2>PTO Worker Homepage</h2>
+    <a href="/welcome">Welcome page Link</a>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+    //return redirect('welcome'); //to redirect to a page
+
 });
+Route::get('/welcome', function () {
+    return view('welcome');
+   
+    
+});
+Route::view("index",'home');
+// short route syntax, 1st parameter URL, second parameter page name in blade template
+
