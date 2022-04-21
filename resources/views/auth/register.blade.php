938c8969d0eee19c845689d1fc06d5070fbd20e2
rifatshampod <shampodrifat@gmail.com>
2022-04-22 03:48:24 +0800

    login and register function added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,112 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<!DOCTYPE html>
+<html lang="en">
+  <!-- link and header data --> 
+  <x-assets title="Create account : "/>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+  <body>
+    <!----------------------Sing Up start------------------------->
+    <section class="signUp">
+      <div class="container">
+        <div class="signUpContent d-flex align-items-center justify-content-center p-50">
+          <div class="signUpContentDiv">
+            <div class="singUpImg cursor mx-auto mb-4" onclick="location.href='index.html'">
+              <img src="assets/image/logo.png" alt="" />
             </div>
+            <div class="signUpFeild bg-white">
+              <div class="mb-3">
+                <h3 class="cl-mat-black fw-bold">Sing Up</h3>
+              </div>
+              <div>
+                  
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                  <div class="signInFeildBox">
+                    <input type="text"
+                    class="w-100 mb-3 py-2 px-3 rounded-3 border-0 bg-cl-ash2 @error('name') is-invalid @enderror"
+                     name="name" value="{{ old('name') }}" required autocomplete="name" 
+                     autofocus  placeholder="Full Name"/>
+                     @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                  </div>
+
+                  <div class="signInFeildBox">
+                    
+                    <input id="email" type="email" placeholder="Email address"
+                    class="w-100 mb-3 py-2 px-3 rounded-3 border-0 bg-cl-ash2 @error('email') is-invalid @enderror" 
+                    name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+
+                  </div>
+
+                  <div class="signInFeildBox">
+                    <input class="w-100 mb-3 py-2 px-3 rounded-3 border-0 bg-cl-ash2"  
+                    type="number" name="phone" placeholder="Phone Number" />
+                  </div>
+
+                  <div>
+                    <select name="country"
+                      class="form-select border-0 mb-3 rounded-3 cl-grey bg-cl-ash2"
+                      aria-label="Default select example">
+                      <option selected>Select Country</option>
+                      <option value="1">Bangladesh</option>
+                      <option value="2">Denmark</option>
+                      <option value="3">Germany</option>
+                    </select>
+                  </div>
+                  <div class="signInFeildBox">
+                    <input id="password" type="password" 
+                    class="w-100 mb-3 py-2 px-3 rounded-3 border-0 bg-cl-ash2 @error('password') is-invalid @enderror" 
+                    name="password" required autocomplete="new-password" placeholder="Password">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror                  
+                  </div>
+                  <div class="signInFeildBox">
+                    <input id="password-confirm" placeholder="Confirm Password"
+                    type="password" class="w-100 mb-3 py-2 px-3 rounded-3 border-0 bg-cl-ash2" 
+                    name="password_confirmation" required autocomplete="new-password"> 
+                  </div>
+                  <div>
+                    <div class="form-check mb-2">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+                        <label class="form-check-label" for="flexCheckDefault1">
+                          T.O.S <span class="fs10 cursor cl-pm fw-bold" onclick="location.href='terms.html'">View Terms of Services</span>
+                        </label>
+                      </div>
+                      {{-- <div class="form-check mb-3">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked2" checked>
+                        <label class="form-check-label" for="flexCheckChecked2">
+                            I wish to receive Newsletter
+                        </label>
+                      </div> --}}
+                  </div>
+                  <div class="mb-2">
+                      <button type="submit" class="w-100 py-2 border-0 rounded-3 bg-cl-pm cl-white">Sign Up</button>
+                  </div>
+                </form>
+                <div class="text-center cl-dark-ash2">
+                    <p>Already a Member? <span class="cursor cl-pm fw-bold" onclick="location.href='/login'">Sign In</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-</div>
-@endsection
+      </div>
+    </section>
+    <!----------------------Sing Up End------------------------->
+  </body>
+</html>
