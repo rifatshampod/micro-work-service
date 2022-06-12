0f1b27cd6957eeeb6ade49cf8dfe2fc61b5c705c
rifatshampod <shampodrifat@gmail.com>
2022-06-12 18:28:44 +0800

    rating modified

--- resources/views/singleGig.blade.php
+++ resources/views/singleGig.blade.php
@@ -2,72 +2,145 @@
 <html lang="en">
   <!-- link and header data --> 
   <x-assets title="Create gig : "/>
+  {{-- <style>
+          .rating {
+        display: inline-block;
+        position: relative;
+        height: 50px;
+        line-height: 50px;
+        font-size: 50px;
+      }
+
+      .rating label {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        cursor: pointer;
+      }
+
+      .rating label:last-child {
+        position: static;
+      }
+
+      .rating label:nth-child(1) {
+        z-index: 5;
+      }
+
+      .rating label:nth-child(2) {
+        z-index: 4;
+      }
+
+      .rating label:nth-child(3) {
+        z-index: 3;
+      }
+
+      .rating label:nth-child(4) {
+        z-index: 2;
+      }
+
+      .rating label:nth-child(5) {
+        z-index: 1;
+      }
+
+      .rating label input {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+      }
+
+      .rating label .icon {
+        float: left;
+        color: transparent;
+      }
+
+      .rating label:last-child .icon {
+        color: #000;
+      }
+
+      .rating:not(:hover) label input:checked ~ .icon,
+      .rating:hover label:hover input ~ .icon {
+        color: #09f;
+      }
+
+      .rating label input:focus:not(:checked) ~ .icon:last-child {
+        color: #000;
+        text-shadow: 0 0 5px #09f;
+      }
+  </style> --}}
   <style>
-    .rating {
-  display: inline-block;
-  position: relative;
-  height: 50px;
-  line-height: 50px;
-  font-size: 50px;
-}
+          .rating {
+        --dir: right;
+        --fill: gold;
+        --fillbg: rgba(100, 100, 100, 0.15);
+        --star: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 17.25l-6.188 3.75 1.641-7.031-5.438-4.734 7.172-0.609 2.813-6.609 2.813 6.609 7.172 0.609-5.438 4.734 1.641 7.031z"/></svg>');
+        --stars: 5;
+        --starsize: 3rem;
+        --symbol: var(--star);
+        --value: 1;
+        --w: calc(var(--stars) * var(--starsize));
+        --x: calc(100% * (var(--value) / var(--stars)));
+        block-size: var(--starsize);
+        inline-size: var(--w);
+        position: relative;
+        touch-action: manipulation;
+        -webkit-appearance: none;
+      }
+      [dir="rtl"] .rating {
+        --dir: left;
+      }
+      .rating::-moz-range-track {
+        background: linear-gradient(to var(--dir), var(--fill) 0 var(--x), var(--fillbg) 0 var(--x));
+        block-size: 100%;
+        mask: repeat left center/var(--starsize) var(--symbol);
+      }
+      .rating::-webkit-slider-runnable-track {
+        background: linear-gradient(to var(--dir), var(--fill) 0 var(--x), var(--fillbg) 0 var(--x));
+        block-size: 100%;
+        mask: repeat left center/var(--starsize) var(--symbol);
+        -webkit-mask: repeat left center/var(--starsize) var(--symbol);
+      }
+      .rating::-moz-range-thumb {
+        height: var(--starsize);
+        opacity: 0;
+        width: var(--starsize);
+      }
+      .rating::-webkit-slider-thumb {
+        height: var(--starsize);
+        opacity: 0;
+        width: var(--starsize);
+        -webkit-appearance: none;
+      }
+      .rating, .rating-label {
+        display: block;
+        font-family: ui-sans-serif, system-ui, sans-serif;
+      }
+      .rating-label {
+        margin-block-end: 1rem;
+      }
 
-.rating label {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  cursor: pointer;
-}
-
-.rating label:last-child {
-  position: static;
-}
-
-.rating label:nth-child(1) {
-  z-index: 5;
-}
-
-.rating label:nth-child(2) {
-  z-index: 4;
-}
-
-.rating label:nth-child(3) {
-  z-index: 3;
-}
-
-.rating label:nth-child(4) {
-  z-index: 2;
-}
-
-.rating label:nth-child(5) {
-  z-index: 1;
-}
-
-.rating label input {
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0;
-}
-
-.rating label .icon {
-  float: left;
-  color: transparent;
-}
-
-.rating label:last-child .icon {
-  color: #000;
-}
-
-.rating:not(:hover) label input:checked ~ .icon,
-.rating:hover label:hover input ~ .icon {
-  color: #09f;
-}
-
-.rating label input:focus:not(:checked) ~ .icon:last-child {
-  color: #000;
-  text-shadow: 0 0 5px #09f;
-}
+      /* NO JS */
+      .rating--nojs::-moz-range-track {
+        background: var(--fillbg);
+      }
+      .rating--nojs::-moz-range-progress {
+        background: var(--fill);
+        block-size: 100%;
+        mask: repeat left center/var(--starsize) var(--star);
+      }
+      .rating--nojs::-webkit-slider-runnable-track {
+        background: var(--fillbg);
+      }
+      .rating--nojs::-webkit-slider-thumb {
+        background-color: var(--fill);
+        box-shadow: calc(0rem - var(--w)) 0 0 var(--w) var(--fill);
+        opacity: 1;
+        width: 1px;
+      }
+      [dir="rtl"] .rating--nojs::-webkit-slider-thumb {
+        box-shadow: var(--w) 0 0 var(--w) var(--fill);
+      }
   </style>
 
   <body>
@@ -228,53 +301,29 @@
                         <small class="cl-grey">{{$gigs->speciality}}</small>
                     </div>
                     <div class="mt-2">
-                        <h4 class="cl-mat-black">Total Ratings: <span class="ms-3 cl-pm">{{collect($ratings)->sum('rating')/collect($ratings)->count('rating')}}</span></h4>
+                        <h4 class="cl-mat-black">Total Ratings: <span class="ms-3 cl-pm">{{round(collect($ratings)->sum('rating')/collect($ratings)->count('rating'),1)}}</span></h4>
                     </div>
                     <!-------------------add rating ------------------>
                     {{-- @if($condition)
                       
                     @endif --}}
-                    <form class="rating" method="post" action="give-rating">
-                      @csrf
-                      <input type="hidden" name="gig_id" value="{{$gigs->id}}">
-                      <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                      <label>
-                        <input type="radio" name="stars" value="1" />
-                        <span class="icon">★</span>
-                      </label>
-                      <label>
-                        <input type="radio" name="stars" value="2" />
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                      </label>
-                      <label>
-                        <input type="radio" name="stars" value="3" />
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>   
-                      </label>
-                      <label>
-                        <input type="radio" name="stars" value="4" />
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                      </label>
-                      <label>
-                        <input type="radio" name="stars" value="5" />
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                        <span class="icon">★</span>
-                      </label>
-                      <br>
-                      {{-- <br>
-                      <label>
-                      <input type="submit" name="submit" value="submit"/>
-                      </label> --}}
-                      <button type="submit">Submit</button>
-                    </form>
+                    <div class="mt-4 text-center">
+                        <form  method="post" action="give-rating">
+                          @csrf
+                          <input type="hidden" name="gig_id" value="{{$gigs->id}}">
+                          <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                          Add Rating
+                          <input
+                              class="rating rating--nojs"
+                              max="5"
+                              step="1"
+                              type="range"
+                              name="stars">
+                          
+                          <button class="px-4 py-2 rounded-3 border-0 bg-cl-pm cl-white mt-2 text-center" type="submit">Submit Rating</button>
+                        </form>
+                    </div>
+                    
                     
 
 
